b911331bede96d41946525971c14099aa535b178
kasperchux <kasperchuk.lrn@gmail.com>
2024-05-29 23:25:13 +0300

    Tiny changes.
    I have cleaned code from useless lines of code and added some comments

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
 from flask import Flask, render_template, request
 from pipe import get_data
+
 app = Flask(__name__, template_folder='frontend', static_folder='frontend')
 
 saved_data = {}
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    video_url = ''
+    video_url = '' # Create variables
     source_text = ''
     translated_text = ''
     summary = ''
 
-    if request.method == 'POST':
+    if request.method == 'POST': 
         video_url = request.form['video_url']
-        print(video_url)
 
-        # Вызываем функцию get_data
+        # Call get_data, to get models predictions
         source_text, translated_text, summary = get_data(video_url)
 
-        # Сохраняем данные в словаре
+        # Save it in the dictionary 
         saved_data[video_url] = {
             'source': source_text,
             'translated': translated_text,
@@ -26,5 +26,6 @@ def index():
         }
 
     return render_template('index.html', video_url=video_url, source_text=source_text, translated_text=translated_text, summary=summary)
-if __name__ == '__main__':
+
+if __name__ == '__main__': # Launch flask app
     app.run(debug=True)
