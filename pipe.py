67b274fb0f98b9acbeb65426f745f29973a048ce
kasperchux <kasperchuk.lrn@gmail.com>
2024-06-17 12:29:16 +0300

    Metrics computed
    In this commit: Metrics for translation and summarization models.

--- pipe.py
+++ pipe.py
@@ -5,6 +5,22 @@ from pydub import AudioSegment
 import os
 import cv2
 import json
+# def get_data(link: str):
+#     stt = pipeline("automatic-speech-recognition", model="openai/whisper-small")
+#     F.get_audio_from_youtube(link)
+#     translation = pipeline("translation", model="Helsinki-NLP/opus-mt-en-ru")
+#     summary = Summarizer.Summarizer("kasperchux/Bart-Base-Summarization", "kasperchux/Bart-Base-Summarization")
+#     summary.build()
+#     # summary = pipeline("summarization", model="Falconsai/text_summarization")
+#     source = stt("audio.wav")["text"]
+#     translated = translation(source)[0]["translation_text"]
+#     summarized = summary.summarize(translated)
+#     t_summarized = translation(summarized)[0]["translation_text"]
+
+#     return source, translated, t_summarized
+
+#     os.remove("audio.wav")
+
 
 def get_data(link: str):
     stt = pipeline("automatic-speech-recognition", model="openai/whisper-small")
